8093ce19f0d97ea593605043461b86c0d692a411
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-21 16:33:27 -0700

    Added local city functionality

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const citySelector = document.querySelector("#city-selector");
 const citiesElement = document.querySelector(".cities");
+let intervalID;
 
 //Los Angeles Elements
 const losAngelesElement = document.querySelector("#los-angeles");
@@ -9,6 +10,14 @@ const losAngelesTime = losAngelesElement.querySelector(".time");
 const denverElement = document.querySelector("#denver");
 const denverDate = denverElement.querySelector(".date");
 const denverTime = denverElement.querySelector(".time");
+//Chicago Elements
+const chicagoElement = document.querySelector("#chicago");
+const chicagoDate = chicagoElement.querySelector(".date");
+const chicagoTime = chicagoElement.querySelector(".time");
+//New York Elements
+const newYorkElement = document.querySelector("#new-york");
+const newYorkDate = newYorkElement.querySelector(".date");
+const newYorkTime = newYorkElement.querySelector(".time");
 
 //function to update date and time
 function updateDateAndTime(dateElement, timeElement, timezone) {
@@ -32,21 +41,48 @@ setInterval(() => {
   updateDateAndTime(denverDate, denverTime, "America/Denver");
 }, 1000);
 
+//Updates Chicago info
+updateDateAndTime(chicagoDate, chicagoTime, "America/Chicago");
+setInterval(() => {
+  updateDateAndTime(chicagoDate, chicagoTime, "America/Chicago");
+}, 1000);
+
+//Updates New York info
+updateDateAndTime(newYorkDate, newYorkTime, "America/New_York");
+setInterval(() => {
+  updateDateAndTime(newYorkDate, newYorkTime, "America/New_York");
+}, 1000);
+
 function updateCity(event) {
-  const timezone = event.target.value;
+  let timezone = event.target.value;
+  if (timezone === "local-city") {
+    timezone = moment.tz.guess();
+  }
   const cityName = timezone.replace("_", " ").split("/")[1];
   const date = moment().tz(timezone).format("MMMM D, YYYY");
   const time = moment()
     .tz(timezone)
     .format(`hh:mm:ss [<span class="am-pm">]A[</span>]`);
   citiesElement.innerHTML = `
-   <div class="city" id="los-angeles">
+   <div class="city" id="selected-city">
           <div class="city-left">
             <h2>${cityName}</h2>
             <div class="date">${date}</div>
           </div>
           <div class="time">${time}</div>
         </div>`;
+
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
+
+  const selectedCity = document.querySelector("#selected-city");
+  const dateElement = selectedCity.querySelector(".date");
+  const timeElement = selectedCity.querySelector(".time");
+  updateDateAndTime(dateElement, timeElement, timezone);
+  intervalID = setInterval(() => {
+    updateDateAndTime(dateElement, timeElement, timezone);
+  }, 1000);
 }
 
 citySelector.addEventListener("change", updateCity);
