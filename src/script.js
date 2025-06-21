2c97fc1e769bc197ac4bfb927d19e9a732399280
pmb9308613 <162384210+pmb9308613@users.noreply.github.com>
2025-06-20 19:57:25 -0700

    Created and implemented updateDateandTime function

--- src/script.js
+++ src/script.js
@@ -6,24 +6,20 @@ const denverElement = document.querySelector("#denver");
 const denverDate = denverElement.querySelector(".date");
 const denverTime = denverElement.querySelector(".time");
 
-losAngelesDate.innerHTML = moment()
-  .tz("America/Los-Angeles")
-  .format("MMMM D, YYYY");
-
-function updateLATime() {
-  losAngelesTime.innerHTML = moment()
-    .tz("America/Los-Angeles")
+function updateDateAndTime(dateElement, timeElement, timezone) {
+  //Date
+  dateElement.innerHTML = moment().tz(timezone).format("MMMM D, YYYY");
+  //Time
+  timeElement.innerHTML = moment()
+    .tz(timezone)
     .format(`hh:mm:ss [<span class="am-pm">]A[</span>]`);
 }
-updateLATime();
-setInterval(updateLATime, 1000);
+updateDateAndTime(losAngelesDate, losAngelesTime, "America/Los-Angeles");
+setInterval(() => {
+  updateDateAndTime(losAngelesDate, losAngelesTime, "America/Los-Angeles");
+}, 1000);
 
-function updateDenverTime() {
-  denverDate.innerHTML = moment().tz("America/Denver").format("MMMM D, YYYY");
-
-  denverTime.innerHTML = moment()
-    .tz("America/Denver")
-    .format(`hh:mm:ss [<span class="am-pm">]A[</span>]`);
-}
-updateDenverTime();
-setInterval(updateDenverTime, 1000);
+updateDateAndTime(denverDate, denverTime, "America/Denver");
+setInterval(() => {
+  updateDateAndTime(denverDate, denverTime, "America/Denver");
+}, 1000);
